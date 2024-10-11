e2ae1cad85ac6addaf788ce4404c3ed3e0e0abb8
paul <paul.sajo.official@gmail.com>
2024-10-11 12:20:17 +0530

    added firstpersonPlayer as seperate class

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { Octree } from 'three/addons/math/Octree.js';
 import { OctreeHelper } from 'three/addons/helpers/OctreeHelper.js';
 
-import { Capsule } from 'three/addons/math/Capsule.js';
-
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
 import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
 import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
+import FirstPersonPlayer from './control';
 
 
 const clock = new THREE.Clock();
@@ -25,11 +24,13 @@ const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerH
 camera.rotation.order = 'YXZ';
 camera.position.set(0, 10, 0)
 
-const light = new THREE.AmbientLight("#fff", 2);
+const light = new THREE.AmbientLight("#fff", 5);
 scene.add(light);
 
 
 const container = document.getElementById('model-container');
+container.tabIndex = 0;
+container.focus()
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -41,63 +42,24 @@ renderer.toneMapping = THREE.ACESFilmicToneMapping;
 container.appendChild(renderer.domElement);
 
 const cssRenderer = new CSS2DRenderer();
+cssRenderer.domElement.style.position = 'absolute';
+cssRenderer.domElement.style.top = '0';
 cssRenderer.setSize(container.innerWidth, container.innerHeight);
-// container.appendChild(cssRenderer.domElement)
+container.appendChild(cssRenderer.domElement);
 
 const stats = new Stats();
 stats.domElement.style.position = 'absolute';
 stats.domElement.style.top = '0px';
-container.appendChild(stats.domElement);
+document.body.appendChild(stats.domElement);
 
-const GRAVITY = 30;
 
 const STEPS_PER_FRAME = 5;
 
+// FIXME: the model is only loaded when window is resized
 
 const worldOctree = new Octree();
 
-const playerCollider = new Capsule(new THREE.Vector3(0, 0.35, 0), new THREE.Vector3(0, 1, 0), 0.35);
-
-const playerVelocity = new THREE.Vector3();
-const playerDirection = new THREE.Vector3();
-
-let playerOnFloor = false;
-let mouseTime = 0;
-
-const keyStates = {};
-
-
-document.addEventListener('keydown', (event) => {
-
-    keyStates[event.code] = true;
-
-});
-
-document.addEventListener('keyup', (event) => {
-
-    keyStates[event.code] = false;
-
-});
-
-container.addEventListener('mousedown', () => {
-
-    document.body.requestPointerLock();
-
-    mouseTime = performance.now();
-
-});
-
-
-document.body.addEventListener('mousemove', (event) => {
-
-    if (document.pointerLockElement === document.body) {
-
-        camera.rotation.y -= event.movementX / 500;
-        camera.rotation.x -= event.movementY / 500;
-
-    }
-
-});
+let fpView;
 
 window.addEventListener('resize', onWindowResize);
 
@@ -107,122 +69,10 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     renderer.setSize(container.clientWidth, container.clientHeight);
+    cssRenderer.setSize(container.innerWidth, container.innerHeight);
 
 }
 
-
-function playerCollisions() {
-
-    const result = worldOctree.capsuleIntersect(playerCollider);
-
-    playerOnFloor = false;
-
-    if (result) {
-
-        playerOnFloor = result.normal.y > 0;
-
-        if (!playerOnFloor) {
-
-            playerVelocity.addScaledVector(result.normal, - result.normal.dot(playerVelocity));
-
-        }
-
-        if (result.depth >= 1e-10) {
-
-            playerCollider.translate(result.normal.multiplyScalar(result.depth));
-
-        }
-
-    }
-
-}
-
-function updatePlayer(deltaTime) {
-
-    let damping = Math.exp(- 4 * deltaTime) - 1;
-
-    if (!playerOnFloor) {
-
-        playerVelocity.y -= GRAVITY * deltaTime;
-
-        // small air resistance
-        damping *= 0.1;
-
-    }
-
-    playerVelocity.addScaledVector(playerVelocity, damping);
-
-    const deltaPosition = playerVelocity.clone().multiplyScalar(deltaTime);
-    playerCollider.translate(deltaPosition);
-
-    playerCollisions();
-
-    camera.position.copy(playerCollider.end);
-
-}
-
-
-function getForwardVector() {
-
-    camera.getWorldDirection(playerDirection);
-    playerDirection.y = 0;
-    playerDirection.normalize();
-
-    return playerDirection;
-
-}
-
-function getSideVector() {
-
-    camera.getWorldDirection(playerDirection);
-    playerDirection.y = 0;
-    playerDirection.normalize();
-    playerDirection.cross(camera.up);
-
-    return playerDirection;
-
-}
-
-function controls(deltaTime) {
-
-    // gives a bit of air control
-    const speedDelta = deltaTime * (playerOnFloor ? 25 : 8);
-
-    if (keyStates['KeyW']) {
-
-        playerVelocity.add(getForwardVector().multiplyScalar(speedDelta));
-
-    }
-
-    if (keyStates['KeyS']) {
-
-        playerVelocity.add(getForwardVector().multiplyScalar(- speedDelta));
-
-    }
-
-    if (keyStates['KeyA']) {
-
-        playerVelocity.add(getSideVector().multiplyScalar(- speedDelta));
-
-    }
-
-    if (keyStates['KeyD']) {
-
-        playerVelocity.add(getSideVector().multiplyScalar(speedDelta));
-
-    }
-
-    if (playerOnFloor) {
-
-        if (keyStates['Space']) {
-
-            playerVelocity.y = 15;
-
-        }
-
-    }
-
-}
 
 const loader = new GLTFLoader().setPath('/assets/');
 
@@ -230,54 +80,59 @@ loader.load('art_gallery/scene.gltf', (gltf) => {
 
     scene.add(gltf.scene);
 
-    console.log("Loded")
 
-    worldOctree.fromGraphNode(gltf.scene);
+    const annotationDiv = document.createElement('div');
+    annotationDiv.className = 'annotation';
+    annotationDiv.style.backgroundColor = "red";
+    annotationDiv.textContent = '3D Annotation';
+    annotationDiv.style.zIndex = 1000;
 
-    gltf.scene.traverse(child => {
+    const label = new CSS2DObject(annotationDiv);
+    gltf.scene.add(label);
+    label.position.set(1, 2, 1); // Position the label above the cube
 
-        if (child.isMesh) {
+    // camera.lookAt(label.position)
 
-            child.castShadow = true;
-            child.receiveShadow = true;
+    // gltf.scene.traverse(child => {
 
-            if (child.material.map) {
+    //     if (child.isMesh) {
 
-                child.material.map.anisotropy = 4;
+    //         child.castShadow = true;
+    //         child.receiveShadow = true;
 
-            }
+    //         if (child.material.map) {
 
-        }
+    //             child.material.map.anisotropy = 4;
 
-    });
+    //         }
 
-    const helper = new OctreeHelper(worldOctree);
-    helper.visible = false;
-    scene.add(helper);
+    //     }
+    // });
 
-    const gui = new GUI({ width: 200 });
-    gui.add({ debug: false }, 'debug')
-        .onChange(function (value) {
+    // const helper = new OctreeHelper(worldOctree);
+    // helper.visible = false;
+    // scene.add(helper);
 
-            helper.visible = value;
+    onWindowResize();
 
-        });
+    fpView = new FirstPersonPlayer(camera, scene, container)
+
+    fpView.loadOctaTree(gltf.scene)
+    // worldOctree.fromGraphNode(gltf.scene);
+
+    fpView.updatePlayer(0.01);
+
+
+    // const gui = new GUI({ width: 200 });
+    // gui.add({ debug: false }, 'debug')
+    //     .onChange(function (value) {
+
+    //         helper.visible = value;
+
+    //     });
 
 });
 
-function teleportPlayerIfOob() {
-
-    if (camera.position.y <= - 25) {
-
-        playerCollider.start.set(0, 0.35, 0);
-        playerCollider.end.set(0, 1, 0);
-        playerCollider.radius = 0.35;
-        camera.position.copy(playerCollider.end);
-        camera.rotation.set(0, 0, 0);
-
-    }
-
-}
 
 
 function animate() {
@@ -289,18 +144,12 @@ function animate() {
 
     for (let i = 0; i < STEPS_PER_FRAME; i++) {
 
-        controls(deltaTime);
-
-        updatePlayer(deltaTime);
-
-        // updateSpheres(deltaTime);
-
-        teleportPlayerIfOob();
-
+        fpView?.update(deltaTime)
     }
 
+    cssRenderer.render(scene, camera);
     renderer.render(scene, camera);
-
+    
     stats.update();
 
 }
