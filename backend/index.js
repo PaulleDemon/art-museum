8c073b0010f0d0ff741de2500e41d1b094b24171
paul <paul.sajo.official@gmail.com>
2024-10-14 03:06:28 +0530

    removed conosole

--- backend/index.js
+++ backend/index.js
@@ -130,12 +130,12 @@ const uploadToPinata = async (file) => {
 
     const newFileName = `${fileName}_${timestamp}.${extension}`;
 
-	console.log("File name: ", newFileName)
+	// console.log("File name: ", newFileName)
 	file = new File([file.buffer], newFileName, { type: file.mimetype })
 
 	try {
 		const upload = await pinata.upload.file(file);
-		console.log("Upload hash: ", upload)
+		// console.log("Upload hash: ", upload)
 
 		return upload.IpfsHash
 
